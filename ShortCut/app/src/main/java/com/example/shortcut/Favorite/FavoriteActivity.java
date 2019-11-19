7f627d2cb915fa5f67b7517ef280743a629f5a63
expresshighway <03wow22@gmail.com>
2019-11-20 00:35:18 +0900

    Make Main #1

---
+++ ShortCut/app/src/main/java/com/example/shortcut/Favorite/FavoriteActivity.java
@@ -0,0 +1,16 @@
+package com.example.shortcut.Favorite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shortcut.R;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favoritelist);
+    }
+}
