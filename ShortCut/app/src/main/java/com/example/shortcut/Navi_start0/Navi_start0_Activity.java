267ae02fefb98bb7dc0bbd1cfd7e495ef602047e
expresshighway <03wow22@gmail.com>
2019-11-20 16:11:52 +0900

    Navi0, main completed

--- ShortCut/app/src/main/java/com/example/shortcut/Navi_start0/Navi_start0_Activity.java
+++ ShortCut/app/src/main/java/com/example/shortcut/Navi_start0/Navi_start0_Activity.java
@@ -1,4 +1,4 @@
-package com.example.shortcut.Main;
+package com.example.shortcut.Navi_start0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.shortcut.R;
 
-public class MainActivity extends AppCompatActivity {
+public class Navi_start0_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navi_start0_);
     }
 }
